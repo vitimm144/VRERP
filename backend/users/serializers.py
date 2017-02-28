2f7c9d45c3581399b6adef2d68253cb707eafbd0
victor <vitimm144@gmail.com>
2017-02-28 14:28:09 -0300

    Criado endpoint para cadastro de usuário.

---
+++ backend/users/serializers.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url, include
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
+
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'is_staff', 'password')
---
+++ backend/users/views.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from users.serializers import UserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
